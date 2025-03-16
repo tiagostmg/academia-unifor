2593ee88710e8073e96f14065517d8ebe3dfde83
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2025-03-16 10:46:17 -0300

    Add image rotation feature to LoginScreen with animated background

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:academia_unifor/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
@@ -13,6 +14,38 @@ class LoginScreen extends StatefulWidget {
 class LoginScreenState extends State<LoginScreen> {
   final TextEditingController _userController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+
+  final List<String> imagePaths = [
+    'assets/001.jpg',
+    'assets/002.jpg',
+    'assets/003.jpg',
+    'assets/004.jpg',
+    'assets/005.jpg',
+    'assets/006.jpg',
+  ];
+
+  int _currentImageIndex = 0;
+  double _opacity = 1.0;
+  late Timer _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    _startImageRotation();
+  }
+
+  void _startImageRotation() {
+    _timer = Timer.periodic(const Duration(seconds: 5), (timer) {
+      setState(() => _opacity = 0.0);
+
+      Future.delayed(const Duration(milliseconds: 500), () {
+        setState(() {
+          _currentImageIndex = (_currentImageIndex + 1) % imagePaths.length;
+          _opacity = 1.0;
+        });
+      });
+    });
+  }
 
   void _login() {
     String username = _userController.text.trim();
@@ -34,94 +67,105 @@ class LoginScreenState extends State<LoginScreen> {
   }
 
   @override
-  Widget build(BuildContext context) {
-    final colorScheme = Theme.of(context).colorScheme;
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
 
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [colorScheme.primary, colorScheme.secondary],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-          ),
-        ),
-        child: Center(
-          child: FadeInUp(
-            duration: const Duration(milliseconds: 800),
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Card(
-                elevation: 0,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
+      body: Stack(
+        children: [
+          AnimatedOpacity(
+            duration: const Duration(milliseconds: 500),
+            opacity: _opacity,
+            child: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(imagePaths[_currentImageIndex]),
+                  fit: BoxFit.cover,
                 ),
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      const Text(
-                        "Entrar",
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      const SizedBox(height: 10),
-                      const Text("Por favor, faça login na sua conta."),
-                      const SizedBox(height: 20),
-                      TextField(
-                        controller: _userController,
-                        keyboardType: TextInputType.number,
-                        textInputAction: TextInputAction.next,
-                        decoration: InputDecoration(
-                          labelText: 'Usuário',
-                          prefixIcon: const Icon(Icons.person),
-                          border: const OutlineInputBorder(),
-                        ),
-                      ),
-                      const SizedBox(height: 10),
-                      TextField(
-                        controller: _passwordController,
-                        obscureText: true,
-                        textInputAction: TextInputAction.done,
-                        decoration: InputDecoration(
-                          labelText: 'Senha',
-                          prefixIcon: const Icon(Icons.lock),
-                          border: const OutlineInputBorder(),
-                          suffixIcon: IconButton(
-                            icon: const Icon(Icons.visibility_off),
-                            onPressed: () {},
+              ),
+            ),
+          ),
+          Center(
+            child: FadeInUp(
+              duration: const Duration(milliseconds: 800),
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Card(
+                  elevation: 0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        const Text(
+                          "Entrar",
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
                           ),
                         ),
-                      ),
-                      const SizedBox(height: 20),
-                      ElasticIn(
-                        child: SizedBox(
-                          width: double.infinity,
-                          child: CustomButton(
-                            text: "Entrar",
-                            icon: Icons.login,
-                            onPressed: _login,
+                        const SizedBox(height: 10),
+                        const Text("Por favor, faça login na sua conta."),
+                        const SizedBox(height: 20),
+                        TextField(
+                          controller: _userController,
+                          keyboardType: TextInputType.number,
+                          textInputAction: TextInputAction.next,
+                          decoration: InputDecoration(
+                            labelText: 'Usuário',
+                            prefixIcon: const Icon(Icons.person),
+                            border: const OutlineInputBorder(),
                           ),
                         ),
-                      ),
-                      const SizedBox(height: 10),
-                      TextButton(
-                        onPressed: () {},
-                        child: const Text(
-                          "Não tem uma conta? Dirija-se à recepção.",
-                          style: TextStyle(color: Colors.blue),
+                        const SizedBox(height: 10),
+                        TextField(
+                          controller: _passwordController,
+                          obscureText: true,
+                          textInputAction: TextInputAction.done,
+                          decoration: InputDecoration(
+                            labelText: 'Senha',
+                            prefixIcon: const Icon(Icons.lock),
+                            border: const OutlineInputBorder(),
+                            suffixIcon: IconButton(
+                              icon: const Icon(Icons.visibility_off),
+                              onPressed: () {},
+                            ),
+                          ),
                         ),
-                      ),
-                    ],
+                        const SizedBox(height: 20),
+                        ElasticIn(
+                          child: SizedBox(
+                            width: double.infinity,
+                            child: CustomButton(
+                              text: "Entrar",
+                              icon: Icons.login,
+                              onPressed: _login,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        TextButton(
+                          onPressed: () {},
+                          child: const Text(
+                            "Não tem uma conta? Dirija-se à recepção.",
+                            style: TextStyle(color: Colors.blue),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
             ),
           ),
-        ),
+        ],
       ),
     );
   }
