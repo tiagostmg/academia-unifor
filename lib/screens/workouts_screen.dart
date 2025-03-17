3ee386e34d481ac85661d20a65ac2851b026bf19
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2025-03-16 21:47:08 -0300

    Add equipment suggestion chips to WorkoutsBody for improved user interaction

--- lib/screens/workouts_screen.dart
+++ lib/screens/workouts_screen.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:academia_unifor/widgets.dart';
 
+Map<String, int> suggestions = {
+  "Máquinas para treinamento de força": 40,
+  "Aparelhos ergométricos (cárdio)": 24,
+  "Esteiras": 12,
+  "Bikes para atividades de spinning": 11,
+};
+
 class WorkoutsScreen extends StatelessWidget {
   const WorkoutsScreen({super.key});
 
@@ -29,14 +36,41 @@ class WorkoutsBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(16),
-      child: Align(
-        alignment: Alignment.topLeft,
-        child: Text(
-          'A Academia Unifor oferece modalidades como circuito funcional, lutas, pilates, avaliação nutricional e física.',
-          textAlign: TextAlign.justify,
-        ),
+    final isDarkMode = Theme.of(context).brightness == Brightness.dark;
+    final chipColor = isDarkMode ? Colors.grey[800]! : Colors.grey[200]!;
+    final textColor = isDarkMode ? Colors.white : Colors.black;
+
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Icon(Icons.search, size: 80, color: Colors.grey),
+          const SizedBox(height: 16),
+          const Text(
+            "Pesquise por um treino...",
+            style: TextStyle(fontSize: 18, color: Colors.grey),
+          ),
+          const SizedBox(height: 20),
+          Wrap(
+            alignment: WrapAlignment.center,
+            spacing: 10,
+            runSpacing: 10,
+            children:
+                suggestions.keys
+                    .map(
+                      (equipamento) => Chip(
+                        label: Text(equipamento),
+                        backgroundColor: chipColor,
+                        labelStyle: TextStyle(color: textColor),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                          side: BorderSide(color: Colors.transparent),
+                        ),
+                      ),
+                    )
+                    .toList(),
+          ),
+        ],
       ),
     );
   }
