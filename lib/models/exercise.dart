09b1f5509af881d6d8081523020ead9a73a9108f
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2025-03-29 10:49:44 -0300

    Add notes field to Exercise model and update mock data

--- lib/models/exercise.dart
+++ lib/models/exercise.dart
@@ -1,14 +1,23 @@
 class Exercise {
   String name;
   String reps;
+  String? notes;
 
-  Exercise({required this.name, required this.reps});
+  Exercise({
+    required this.name,
+    required this.reps,
+    this.notes, // Permitir que seja nulo
+  });
 
   factory Exercise.fromJson(Map<String, dynamic> json) {
-    return Exercise(name: json['name'], reps: json['reps']);
+    return Exercise(
+      name: json['name'],
+      reps: json['reps'],
+      notes: json['notes'],
+    );
   }
 
   Map<String, dynamic> toJson() {
-    return {'name': name, 'reps': reps};
+    return {'name': name, 'reps': reps, if (notes != null) 'notes': notes};
   }
 }
